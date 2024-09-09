6b4892c793d069dd9a52cc846bd0fc7faaaccf32
Gufard-0 <gufard@hotmail.com>
2024-09-09 17:47:44 -0300

    tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudar classe String (representa texto)
+
+        String meuNome = "Gustavo";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+
+        final double VALOR_PI = 3.14;
+
+
     }
 }
